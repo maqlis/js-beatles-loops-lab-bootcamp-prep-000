4d97b989b3202eb2dcf5f4ed16bc7abecbffb1ba
Małgorzata Klisińska <klis.mal@gmail.com>
2017-10-23 17:10:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var plays = []
   for( var i = 0; i < musicians.length; i++){
-    plays.push(`${musicians[i]} plays ${instruments[i]}`)
+    plays[i] = (`${musicians[i]} plays ${instruments[i]}`)
   }
+  return plays
 }
